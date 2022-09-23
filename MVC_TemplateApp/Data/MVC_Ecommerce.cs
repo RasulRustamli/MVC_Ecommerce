5b6321b05ac24cc82f3037388603e34f1089f92b
RasulRustamli <rasular@code.edu.az>
2022-09-23 09:00:11 +0400

    Add project files.

---
+++ MVC_TemplateApp/Data/MVC_Ecommerce.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_TemplateApp.Models;
+using System;
+
+namespace MVC_TemplateApp.Data
+{
+    public class MVC_Ecommerce : DbContext
+    {
+        public MVC_Ecommerce(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductPhoto> ProductPhotos { get; set; }
+    }
+}
